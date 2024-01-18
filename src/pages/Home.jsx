8b913e990c0317975128df0d012df3c0283dde6d
mohamed-taj <mohamedtaj0099@gmail.com>
2024-01-18 22:21:46 +0300

    fixed bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import homeStore from "../store/HomeStore";
+import homeStore from "../store/homeStore.js";
 import { Link } from "react-router-dom";
 import Header from "../componants/Header";
 import "./home.css";
